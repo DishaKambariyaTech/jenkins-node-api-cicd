d87113135190522c116cb085c09f234c31137ddf
DishaKambariyaTech <144014669+DishaKambariyaTech@users.noreply.github.com>
2023-11-09 10:41:48 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.get("/hello", (_req, res) => {
+  res.status(200).send("Welcome to our website");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
