59a071f7e1c06d8d88eb221108f19f236b7db8cf
DishaKambariyaTech <144014669+DishaKambariyaTech@users.noreply.github.com>
2023-11-21 16:25:36 +0530

    api added

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.get("/hello", (_req, res) => {
   res.status(200).send("Welcome to our new website");
 });
 
+app.get("/test", (_req, res) => {
+  res.status(200).send("Welcome to our test website");
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
